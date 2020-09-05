d3f36acecdf79eda294f90c9abe305b8b30f09fb
Glenn <gletschert@hotmail.com>
2020-09-05 15:35:26 +0200

    Array Methods

---
+++ Week03/ArrayMethods/ArrayMethods.js
@@ -0,0 +1,62 @@
+//   const addTheWordCool = function(array){
+//       array.push("cool")
+//       return array;
+// }
+// console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+// resultaat: ["nice", "awesome", "tof", "cool"]
+
+
+
+// Deel B
+
+// const amountOfElementsInArray = (element) => {
+//     return element.length
+// }
+// console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen']));
+// 3
+
+
+// Deel C
+
+// const selectBelgiumFromBenelux = (element) => {
+//     return element.shift();
+// }
+// console.log(selectBelgiumFromBenelux(["Belgie", "Nederland", "Luxemburg"])); 
+// resultaat: "Belgie"
+
+
+// Deel D
+
+// const lastElementInArray = element => { return element.pop()}
+
+// console.log(lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"])); 
+// resultaat: "Schildpad"
+
+
+// Deel E
+
+// const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+
+// const impeachTrumpSlice = function(array) {
+//     return array.slice();
+// }
+// const impeachTrumpSplice = function(array) {
+//     return array.splice(); // mutation
+// }
+
+// console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+// console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+
+// Deel F
+
+// const stringsTogether = (element) => { return element.join(' ');}
+// console.log(stringsTogether(['Winc', 'Academy', 'is', 'super', 'leuk', ';-}'])) 
+//resultaat: "Winc Academy is leuk ;-}"
+
+// Deel G
+
+// const combineArrays = (array1, array2) => { return array1.concat(array2); };
+// combineArrays([1, 2, 3], [4, 5, 6])
+
+  // resultaat: [1,2,3,4,5,6]
