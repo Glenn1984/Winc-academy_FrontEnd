341e11a07db5f5051524a484b744762ff6fe172b
Glenn <gletschert@hotmail.com>
2020-09-08 12:50:28 +0200

    Dom manipulation Big five

---
+++ Week03/DomManiOpdracht/script.js
@@ -0,0 +1,32 @@
+// Function for textContent and creating the lists.
+const oneOfBigFive = (element) => {
+    const oneOfBigFiveAnimal = element.target.textContent;
+    const newLi = document.createElement('li');
+    newLi.classList.add('spotted-animals-list-item')
+    newLi.appendChild(document.createTextNode(oneOfBigFiveAnimal))
+    const menu = document.getElementById('spotted-animals-list')
+    menu.appendChild(newLi);
+}
+
+// Event listener for big five buttons.
+const bigFiveButton = document.querySelectorAll('.big-five-button')
+bigFiveButton.forEach((list) => {
+    list.addEventListener('click', oneOfBigFive);
+});
+
+
+// Delete button
+const deleteButton = document.getElementById('remove-first-item-button');
+deleteButton.addEventListener('click', () => {
+    const parent = document.getElementById('spotted-animals-list');
+        const child = parent.getElementsByTagName('li')[0];
+        parent.removeChild(child)
+})
+
+
+// Delete all button
+const deleteButtonAll = document.getElementById('remove-all-button')
+deleteButtonAll.addEventListener('click', () => {
+    const parent = document.getElementById('spotted-animals-list');
+    parent.innerHTML = '';
+})
