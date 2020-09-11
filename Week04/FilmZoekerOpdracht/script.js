3dc7c95f773fbb5b5c19027878ad1b31c2b935f4
Glenn <gletschert@hotmail.com>
2020-09-11 23:41:16 +0200

    Film Searcher adds

--- Week04/FilmZoekerOpdracht/script.js
+++ Week04/FilmZoekerOpdracht/script.js
@@ -6,8 +6,10 @@ const movieContainer = document.getElementById('movieContainer');
 const moviesWithLi = movies.map((movie) => {
     const li_Tag = document.createElement('li');
     li_Tag.classList.add('movieContainer--item');
+    const a_Tag = document.createElement('a');
+    a_Tag.href = "https://www.imdb.com/title/" + movie.imdbID;
     const img_Tag = document.createElement('img');
-    li_Tag.appendChild(img_Tag);
+    li_Tag.appendChild(a_Tag).appendChild(img_Tag);
     img_Tag.src = movie.Poster;
     return li_Tag;
 });
@@ -19,22 +21,56 @@ moviesWithLi.forEach((moviesToDom) => {
 
 
 
-const handleOnChangeEvent = ((event) => {
-    console.log(event.target);
 
+const handleOnChangeEvent = ((event) => {
+    // console.log(event.target.value); // werkt perfect.
+    switch (event.target) { // event.target.value stuurt geen gegevens door - event.target wel
+        case latestMovies: {
+            filterLatestMovies(latestMovies);
+            break;
+        } // add brackets when working with the same variable.
+        case avengersMovies: {
+            filterMovies(avengersMovies);
+            break;
+        }
+        case xmenMovies: {
+            console.log("hey ik ben { xmenMovies } film")
+            break;
+        }
+        case princessMovies: {
+            console.log("hey ik ben {princessMovies} film")
+            break;
+        }
+        case batmanMovies: {
+            console.log("hey ik ben { batmanMovies } film")
+            break;
+        }
+        default:
+            console.log("This doesnt work") // slaat aan bij event.target.value
+            break;
+    }
 });
 
-const radioButtons = document.querySelectorAll('input[type=radio][name="moviesFilter"]');
+const radioButtons = document.querySelectorAll('input[type=radio][name=moviesFilter]');
 
 
 radioButtons.forEach(radio => radio.addEventListener('change', handleOnChangeEvent));
 // console.log(radioButtons);
 
-// optie 1 -radio button:
-// var radios = document.querySelectorAll('input[type=radio][name="contact"]');
-// radios.forEach(radio => radio.addEventListener('change', () => alert(radio.value)));
 
 
+const filterMovies = ((wordInMovieTitle) => {
+    // console.log(filterMovies) // geeft geen value, wel target.
+    wordInMovieTitle.filter((accumulator, currentvalue) => {
+        console.log(currentvalue);
+    });
+});
+
+
+
+const filterLatestMovies = (() => {
+        
+});
 
 
 
@@ -53,6 +89,16 @@ searchBar.addEventListener("keyup", e => {
     displayMovies(console.log(filteredMovies));
 });
 
-const displayMovies = (element) => { return element };
+const displayMovies = (element) => {
+    
+    return element;
+};
 // functie en event listener werken, log naar console.
 // De resultaten moeten nog naar scherm gezonden worden.
+
+
+
+
+// optie 1 -radio button:
+// var radios = document.querySelectorAll('input[type=radio][name="contact"]');
+// radios.forEach(radio => radio.addEventListener('change', () => alert(radio.value)));
