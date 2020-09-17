d480280294879d0bd6d7f3a165ced963ae8373ed
Glenn <gletschert@hotmail.com>
2020-09-17 15:51:32 +0200

    added Async / Await / Fetch

--- Week05/Callbacks/script.js
+++ Week05/Callbacks/script.js
@@ -30,5 +30,36 @@ function createPost(post) { // callback is called right after the array is pushe
 };
 
 
-createPost({ title: 'Post Three', body: 'This is post three' })
-.then(getPosts);
+// createPost({ title: 'Post Three', body: 'This is post three' })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
+
+// Async / Await
+// async function init() {
+//     await createPost({ title: 'Post Three', body: 'This is post three' })
+//     getPosts();
+// };
+
+// init();
+
+// Async / Await / Fetch
+async function fetchUsers() {
+    const res = await fetch
+        ('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    console.log(data);
+};
+fetchUsers();
+
+// Promise.all
+// const promise1 = Promise.resolve("Hello World");
+// const promise2 = 10;
+// const promise3 = new Promise ((resolve, reject) =>
+//     setTimeout(resolve, 2000, 'Goodbye')
+// );
+
+// const promise4 = fetch
+//     ('https://jsonplaceholder.typicode.com/users').then((res) => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) => console.log(values));
