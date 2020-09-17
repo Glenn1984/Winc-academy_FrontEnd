5ca0a0c4742d0cef748211c610856e83f04f6b86
Glenn <gletschert@hotmail.com>
2020-09-17 11:08:03 +0200

    update

---
+++ Week05/AsynchronousJS/script.js
@@ -0,0 +1,27 @@
+let a3 = 100;
+setTimeout(function () { a3++; }, 0);
+console.log(a3); // als eerste uitgevoerd.....
+setTimeout(() => { console.log(a3) }, 0); // Als vierde uitgevoerd.....
+
+let p = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve("Yo!");
+    }, 0);
+});
+console.log(p); // Als tweede uitgevoerd.....
+setTimeout(function () {
+    console.log(p); // Als zevende uitgevoerd....
+}, 10);
+console.log(p); // Als derde uitgevoerd.....
+
+p.then(function (val) {
+    console.log(val); // Als zesde uitgevoerd.....
+});
+
+
+/**
+All Asynchronous JS
+setTimeout, callbacks for geolocation, Promises,
+fetch, ajax, filesystem interaction, database calls,
+even DOM event listeners
+ **/
