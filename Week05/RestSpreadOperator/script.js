79ac481d7d2c3acc71263d47caa28cc7de03886c
Glenn <gletschert@hotmail.com>
2020-09-18 19:36:59 +0200

    Rest and Spread Operator

---
+++ Week05/RestSpreadOperator/script.js
@@ -0,0 +1,17 @@
+// function num(...args) {
+//     return args.reduce((accumulator, currentValue) => {
+//         return accumulator + currentValue;
+//     })
+// }
+
+// console.log(num(1, 2, 3, 4, 5)); // Solution 15
+
+
+// Deel 2
+// const numSolution = function (number1, number2, number3) {
+//     return number1 + number2 + number3;
+// }
+
+// const numbers = [10, 16, 24];
+
+// console.log(numSolution(...numbers));
