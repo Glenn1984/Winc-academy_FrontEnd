cceb7085d627a2560fbb683bff9b11131f51e16b
Glenn <gletschert@hotmail.com>
2020-10-15 21:43:27 +0200

    add new tests

--- Week08/Jest/functions.js
+++ Week08/Jest/functions.js
@@ -6,20 +6,50 @@
 
 
 
-const functions = {
-    isNull: function() {
-      return null;
-    },
-    checkValue: function(x) {
-      return x;
-    },
-    createUser: function() {
-      const user = { 
-        firstName: 'Brad' 
-      };
-      user['lastName'] = 'Traversy';
-      return user;
-    }
-  };
+// const functions = {
+//     isNull: function() {
+//       return null;
+//     },
+//     checkValue: function(x) {
+//       return x;
+//     },
+//     createUser: function() {
+//       const user = { 
+//         firstName: 'Brad' 
+//       };
+//       user['lastName'] = 'Traversy';
+//       return user;
+//     }
+//   };
   
-  module.exports = functions;
+// module.exports = functions;
+  
+//////////////////////////////////////////////////
+
+//! Test Driven Development - Functions
+
+// const addOne = function(numbers) {
+//   // Schrijf hier de functie...
+//   return numbers.map(addOne => addOne + 1);
+// }
+
+// module.exports = addOne;
+
+///////////////////////////////////////////////
+
+
+// const getWordLengths = function(someWords) {
+//   //Schrijf je functie...
+//   return someWords.map(getWord => getWord.length);
+// };
+
+// module.exports = getWordLengths;
+
+////////////////////////////////////////////////////
+
+const findNeedle = function(words, needle) {
+  // Schrijf hier de functie...
+  return words.indexOf(needle);
+};
+
+module.exports = findNeedle;
--- Week08/Jest/functions.test.js
+++ Week08/Jest/functions.test.js
@@ -1,59 +1,104 @@
-const functions = require('./functions.js');
+// const functions = require('./functions.js');
 
-// test('Adds 2 + 2 toEqual 4', () => {
-//     expect(functions.add(2, 2)).toBe(4);
+// // test('Adds 2 + 2 toEqual 4', () => {
+// //     expect(functions.add(2, 2)).toBe(4);
+// // });
+
+// // test('Adds 2 + 2 to NOT Equal 5', () => {
+// //     expect(functions.add(2, 2)).not.toBe(4);
+// // });
+
+
+// test('isNull should be null', () => {
+//     expect(functions.isNull()).toBeNull();
 // });
 
-// test('Adds 2 + 2 to NOT Equal 5', () => {
-//     expect(functions.add(2, 2)).not.toBe(4);
+
+// //////////////////////////////////////////////////
+
+// test('Should be null', () => {
+//         // Begin met expect()
+//         // Binnen de eerste haken zet je hetgeen je wilt testen. Dat zal een functie zijn. 
+//         // Daarna volgt je .toBeSomethingSomething functie, 
+//   // zoek de geschikte functie in de documentatie. 
+//         // In dit geval willen we checken of de functie "isNull()" ook daadwerkelijk 
+//         // Null returned. Dus "toBeNull()" makes the most sense. 
+//   expect(functions.isNull()).toBeNull();
+// });
+
+// test('checkValue Should be falsy when argument is undefined', () => {
+//     expect(functions.checkValue(null)).toBeFalsy();
+// });
+
+// test('User should be Brad Traversy object', () => {
+//       expect(functions.createUser()).toEqual({
+//             firstName: 'Brad',
+//             lastName: 'Traversy'
+//       });
+// });
+
+// // Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
+// // nu tests maken die werken zonder input van buitenaf:
+
+// // Less than or greater than
+// test('Should be under or equal to 1600', () => {
+//       const load1 = 800;
+//       const load2 = 800;
+//       expect(load1 + load2).toBe(1600);
+// });
+
+// // Regex
+// test('There is no I in team', () => {
+//       expect('team').not.toBe();
+// });
+
+// // Arrays
+// test('Admin should be in usernames', () => {
+//  usernames = ['john', 'karen', 'admin'];
+//       expect(usernames).not.toStrictEqual('Admin');
 // });
 
 
-test('isNull should be null', () => {
-    expect(functions.isNull()).toBeNull();
-});
 
+/////////////////////////////////////////////////////
+
+
+//! Test Driven Development - Tests
+
+// const addOne = require("./functions.js");
+
+// test("Add 1 to each item in myArray", function() {
+//   const myArray = [31, 57, 12, 5];
+
+//   const unchanged = [31, 57, 12, 5];
+//   const expected = [32, 58, 13, 6];
+//   const output = addOne(myArray);
+
+//   expect(output).toEqual(expected);
+//   expect(myArray).toEqual(unchanged);
+// });
 
 //////////////////////////////////////////////////
 
-test('Should be null', () => {
-        // Begin met expect()
-        // Binnen de eerste haken zet je hetgeen je wilt testen. Dat zal een functie zijn. 
-        // Daarna volgt je .toBeSomethingSomething functie, 
-  // zoek de geschikte functie in de documentatie. 
-        // In dit geval willen we checken of de functie "isNull()" ook daadwerkelijk 
-        // Null returned. Dus "toBeNull()" makes the most sense. 
-  expect(functions.isNull()).toBeNull();
-});
 
-test('checkValue Should be falsy when argument is undefined', () => {
-    expect(functions.checkValue(null)).toBeFalsy();
-});
+// const getWordLengths = require("./functions.js");
 
-test('User should be Brad Traversy object', () => {
-      expect(functions.createUser()).toEqual({
-            firstName: 'Brad',
-            lastName: 'Traversy'
-      });
-});
+// test("Get word lengths", function() {
+//   const words = ["sun", "potato", "roundabout", "pizza"];
+//   const expected = [3, 6, 10, 5];
 
-// Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
-// nu tests maken die werken zonder input van buitenaf:
+//   const output = getWordLengths(words);
+//   expect(output).toEqual(expected);
+// });
 
-// Less than or greater than
-test('Should be under or equal to 1600', () => {
-      const load1 = 800;
-      const load2 = 800;
-      expect(load1 + load2).toBe(1600);
-});
+//////////////////////////////////////////////////////
 
-// Regex
-test('There is no I in team', () => {
-      expect('team').not.toBe();
-});
+const findNeedle = require("./functions.js");
 
-// Arrays
-test('Admin should be in usernames', () => {
- usernames = ['john', 'karen', 'admin'];
-      expect(usernames).not.toStrictEqual('Admin');
+test("Find the needle", function() {
+  const words = ["house", "train", "slide", "needle", "book"];
+  const expected = 3;
+
+  const output = findNeedle(words, "needle");
+  expect(output).toEqual(expected);
 });
